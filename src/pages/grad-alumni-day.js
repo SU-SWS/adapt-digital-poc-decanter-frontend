c4d94788caaec93e90a82e6daad211d8b7b0a256
Katria Lesser <Devbydesign@mindsing.com>
2020-02-26 15:34:11 -0700

    ADAPT-94: Adjust loop through referenced entity data, improve pricing styles

--- src/pages/grad-alumni-day.js
+++ src/pages/grad-alumni-day.js
@@ -128,6 +128,25 @@ const GradAlumniDay = ({ data, location }) => {
                 <p>Present list of sessions here (title, description, etc)</p>
 
                 {sessions.map(({ node }) => {
+                    const people = []
+                    if(node.sessionPeople !== null) {
+                        for (const [index, value] of node.sessionPeople.entries()) {
+                            people.push(
+                                <div class="session-person">
+                                    <h3>{value.personDisplayName}</h3>
+                                    <div>{value.presentationTitle}</div>
+                                    <div>{value.personTitle}</div>
+                                    <div>{value.personClassYear + "'"}</div>
+                                    {/*var personImage == {node.sessionPeople ? node.sessionPeople[0].personImage.file.url : "no people"}</div>*/}
+                                    <div>{'File url to render next: https:' + value.personImage.file.url}</div>
+                                    {/*<img source={node.sessionPeople ? 'https:' + node.sessionPeople[0].personImage.file.url : ""}>*/}
+                                    {/*    <img src={ require( node.sessionPeople ? 'https:' + node.sessionPeople[0].personImage.file.url ) } />*/}
+                                    <div>{"File alt tag description: " + value.personImage.description}</div>
+                                </div>
+                            )
+                        }
+                    }
+
                     return (
 
                         <article class="event-session--wrapper" key={node.sessionTitle}>
@@ -139,21 +158,7 @@ const GradAlumniDay = ({ data, location }) => {
                             {/*<h3>{node.sessionPeople.personDisplayName}</h3>*/}
                             <div class="session-people">
 
-                                {/*<h3>{node.sessionPeople ? node.sessionPeople[0].personDisplayName : "no people"}</h3>*/}
-                                <h3>{node.sessionPeople ? node.sessionPeople[0].personDisplayName : ""}</h3>
-                                <div>{node.sessionPeople ? node.sessionPeople[0].presentationTitle : ""}</div>
-                                <div>{node.sessionPeople ? node.sessionPeople[0].personTitle : ""}</div>
-                                <div>{node.sessionPeople ? node.sessionPeople[0].personClassYear : ""}</div>
-                                {/*var personImage == {node.sessionPeople ? node.sessionPeople[0].personImage.file.url : "no people"}</div>*/}
-                                <div>{node.sessionPeople ? 'File url to render next: https:' + node.sessionPeople[0].personImage.file.url : ""}</div>
-                                {/*<img source={node.sessionPeople ? 'https:' + node.sessionPeople[0].personImage.file.url : ""}>*/}
-                                {/*    <img src={ require( node.sessionPeople ? 'https:' + node.sessionPeople[0].personImage.file.url ) } />*/}
-                                <div>{node.sessionPeople ? "File alt tag description: " + node.sessionPeople[0].personImage.description : ""}</div>
-
-
-
-
-
+                                {people}
 
                             </div>
                         </article>
@@ -163,54 +168,254 @@ const GradAlumniDay = ({ data, location }) => {
             </div>
 
                 {event.map(({ node }) => {
+
+                    const priceItems = []
+                    if(node.eventPricing !== null) {
+                        for (const [index, value] of node.eventPricing.entries()) {
+                            priceItems.push(
+                                <article className="su-card [ modifier_class ]">
+                                    <section className="su-card__contents">
+                                        <h2>{value.productName}</h2>
+                                        <p>Rich text pricing description goes here.</p>
+                                        <div className="product--price">{"$" + value.productPrice}</div>
+                                        <div className="product--price-type">{value.productPriceType}</div>
+                                    </section>
+                                </article>
+                            )
+                        }
+                    }
+
                     return (
 
                         <div>
                             <article class="event-pricing--wrapper" key={node.eventPricing ? node.eventPricing[0].productName : "Event Pricing"}>
+                                <div class="event-pricing--inner-wrapper text-centered">
                                     <header>
-
                                         <h2 class={"event-pricing"}>Pricing</h2>
-                                        {/*<div>[Rich Text - Session Description Goes Here.]</div>*/}
                                     </header>
-                                    <h3>{node.eventPricing ? node.eventPricing[0].productName : ""}</h3>
-                                    {/* add Rich text Pricing Description*/}
-                                    <p>Rich text pricing description goes here.</p>
-                                    <div>{node.eventPricing ? "$" + node.eventPricing[0].productPrice : ""}</div>
-                                    {/* event > product > title*/}
-                                    {/* event > product > description*/}
-                                    {/* event > product > price*/}
-                                    {/* event > product > whatever field contains 'regular price' text*/}
-
-                                    {/*<h3>{node.eventCardsTop ? node.eventCardsTop[0].cardBody.content.content.value : "no card body"}</h3>*/}
-                                    {/*<h3>{node.sessionPeople ? node.sessionPeople[0].personDisplayName : ""}</h3>*/}
-
-
-                            </article>
-                            < article className = "event-video--wrapper text-centered"  key="Video Section">
-
-                                <div class="watch-video-label">Watch</div>
-                                <h3 class="video-title-label">Video Title: {node.video ?  node.video.title : "Video Title"}</h3>
-
-                                <div>Video url: {node.video ? "https:" + node.video.file.url : "Video Url"}</div>
-                                <div>Rich Text - Video description goes here later</div>
-                                {/*// {node.video.description}*/}
+                                    <div className="price-items--wrapper">
+                                        {priceItems}
+                                    </div>
+                                </div>
                             </article>
 
-                            < article className = "event-quote--wrapper"  key="Quote Section">
+                            <div class="bottom-page-section constrained-width">
 
-                                <h4 class="text-centered">Rich text - Quote testimonialText field goes here</h4>
+                                < article className = "event-video--wrapper text-centered"  key="Video Section">
 
-                                <div className="text-centered">- Quote Author goes here</div>
+                                    <div class="watch-video-label">Watch</div>
+                                    <h3 class="video-title-label">Video Title: {node.video ?  node.video.title : "Video Title"}</h3>
 
-                                {/*<div>Quote Author: {node.quotes ? "https:" + node.quotes[0].testimonialAuthor : "Quote Author"}</div>*/}
+                                    <div>Video url: {node.video ? "https:" + node.video.file.url : "Video Url"}</div>
+                                    <div>Rich Text - Video description goes here later</div>
+                                    {/*// {node.video.description}*/}
+                                </article>
 
-                            </article>
+                                < article className = "event-quote--wrapper"  key="Quote Section">
+
+                                    <h4 class="text-centered">Rich text - Quote testimonialText field goes here</h4>
+
+                                    <div className="text-centered">- Quote Author goes here</div>
+
+                                    {/*<div>Quote Author: {node.quotes ? "https:" + node.quotes[0].testimonialAuthor : "Quote Author"}</div>*/}
+
+                                </article>
 
 
+                            </div>
                         </div>
                     )
                 })}
 
+
+            <div className="su-local-footer [ modifier_class ]">
+                <header className="su-local-footer__header">
+                    <div className="su-lockup su-lockup--option-a">
+                        <div className="su-lockup__cell1">
+                            <div className="su-lockup__wordmark-wrapper">
+                                <span className="su-lockup__wordmark">Stanford</span>
+                            </div>
+                        </div>
+                        <div className="su-lockup__cell2">
+                            <span className="su-lockup__line1">Engineering</span>
+                        </div>
+                    </div>
+                    <a href="/login" className="su-link su-link--internal">Web Login</a>
+                </header>
+                <section className="su-local-footer__columns">
+                    <div className="su-local-footer__cell1">
+                        <address className="su-local-footer__address">
+                            <strong>Shriram Center | Chemical Engineering</strong>
+                            <br/>
+                            443 Via Ortega | Room 129
+                            <br/>
+                            Stanford, CA 94305
+                            <br/>
+                            <a href="tel:+16507234906">650-723-4906</a>
+                            <br/>
+                        </address>
+                        <ul className="su-local-footer__action-links">
+                            <li>
+                                <a href="#">Visit</a>
+                            </li>
+                            <li>
+                                <a href="#">Campus Map</a>
+                            </li>
+                            <li>
+                                <a href="#">Contact Us</a>
+                            </li>
+                        </ul>
+                        <ul className="su-local-footer__social-links">
+                            <li>
+                                <a href="#" className="su-local-footer__social-facebook">
+                                    <i aria-hidden="true"></i>
+                                    <span>Facebook</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" className="su-local-footer__social-linkedin">
+                                    <i aria-hidden="true"></i>
+                                    <span>LinkedIn</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" className="su-local-footer__social-twitter">
+                                    <i aria-hidden="true"></i>
+                                    <span>Twitter</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" className="su-local-footer__social-instagram">
+                                    <i aria-hidden="true"></i>
+                                    <span>Instagram</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" className="su-local-footer__social-youtube">
+                                    <i aria-hidden="true"></i>
+                                    <span>Youtube</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="su-local-footer__cell2">
+                        <nav aria-label="footer primary nav">
+                            <h2 className="su-local-footer__list-heading">Links to</h2>
+                            <ul className="su-local-footer__primary-links">
+                                <li>
+                                    <a href="#">Stanford Engineering Magazine</a>
+                                </li>
+                                <li>
+                                    <a href="#">Open Faculty Positions</a>
+                                </li>
+                                <li>
+                                    <a href="#">News</a>
+                                </li>
+                                <li>
+                                    <a href="#">Events</a>
+                                </li>
+                                <li>
+                                    <a href="#">Colloquium</a>
+                                </li>
+                                <li>
+                                    <a href="#">Give</a>
+                                </li>
+                            </ul>
+                        </nav>
+                        <nav aria-label="footer secondary nav">
+                            <h2 className="su-local-footer__list-heading">Resources for</h2>
+                            <ul className="su-local-footer__secondary-links">
+                                <li>
+                                    <a href="#">Current Graduate Students</a>
+                                </li>
+                                <li>
+                                    <a href="#">Current Undergraduate Students</a>
+                                </li>
+                                <li>
+                                    <a href="#">Faculty & Staff</a>
+                                </li>
+                                <li>
+                                    <a href="#">Alumni</a>
+                                </li>
+                                <li>
+                                    <a href="#">Intranet</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div className="su-local-footer__cell3">
+                        <div className="su-signup-form [ modifier_class ]">
+                            <h2 className="su-local-footer__list-heading">Sign up for our email.</h2>
+                            <p>Your source for engineering research and ideas</p>
+                            <form action="/" method="post">
+                                <input type="email" id="su-email" aria-label="email address" name="su-email"
+                                       placeholder="email address" required=""/>
+                                <input type="submit" id="su-submit" name="su-submit" value="Sign-up"/>
+                            </form>
+                        </div>
+                    </div>
+                </section>
+            </div>
+
+            <section className="su-global-footer [ modifier_class ]">
+                <div className="su-global-footer__container">
+                    <div className="su-global-footer__brand">
+                        <a className="su-logo [ modifier_class ]" href="https://www.stanford.edu">
+                            Stanford
+                            <br/>
+                            University
+                        </a>
+                    </div>
+                    <div className="su-global-footer__content">
+                        <nav aria-label="global footer menu">
+                            <ul className="su-global-footer__menu su-global-footer__menu--global">
+                                <li>
+                                    <a href="https://www.stanford.edu">Stanford Home</a>
+                                </li>
+                                <li>
+                                    <a href="https://visit.stanford.edu/plan/">Maps &amp; Directions</a>
+                                </li>
+                                <li>
+                                    <a href="https://www.stanford.edu/search/">Search Stanford</a>
+                                </li>
+                                <li>
+                                    <a href="https://emergency.stanford.edu">Emergency Info</a>
+                                </li>
+                            </ul>
+                            <ul className="su-global-footer__menu su-global-footer__menu--policy">
+                                <li>
+                                    <a href="https://www.stanford.edu/site/terms/" title="Terms of use for sites">Terms
+                                        of Use</a>
+                                </li>
+                                <li>
+                                    <a href="https://www.stanford.edu/site/privacy/"
+                                       title="Privacy and cookie policy">Privacy</a>
+                                </li>
+                                <li>
+                                    <a href="https://uit.stanford.edu/security/copyright-infringement"
+                                       title="Report alleged copyright infringement">Copyright</a>
+                                </li>
+                                <li>
+                                    <a href="https://adminguide.stanford.edu/chapter-1/subchapter-5/policy-1-5-4"
+                                       title="Ownership and use of Stanford trademarks and images">Trademarks</a>
+                                </li>
+                                <li>
+                                    <a href="http://exploredegrees.stanford.edu/nonacademicregulations/nondiscrimination/"
+                                       title="Non-discrimination policy">Non-Discrimination</a>
+                                </li>
+                                <li>
+                                    <a href="https://www.stanford.edu/site/accessibility"
+                                       title="Report web accessibility issues">Accessibility</a>
+                                </li>
+                            </ul>
+                        </nav>
+                        <div className="su-global-footer__copyright">
+                            <span>&copy; Stanford University.</span>
+                            <span>&nbsp; Stanford, California 94305.</span>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </main>
     )
 }
@@ -258,6 +463,7 @@ query GradAlumniData {
         eventPricing {
           productName
           productPrice 
+          productPriceType
         }
         video {
           title
