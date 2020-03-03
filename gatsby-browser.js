359d37c36127a7248927c9e6fea4e12295a49691
Katria Lesser <Devbydesign@mindsing.com>
2020-03-03 16:52:32 -0700

    ADAPT-94: Fixing decanter integration

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,31 +3,10 @@ import "typeface-montserrat"
 import "typeface-merriweather"
 
 import "prismjs/themes/prism.css"
-import "./src/styles/css/global.css"
-
 
 // Decanter styles
+// I'm importing decanter in the global.scss file because for some reason it wasn't processing correctly here.
 // CSS only version
 // import "./src/styles/css/decanter.css"
 // import "./src/styles/css/decanter-grid.css"
-
-// SCSS version
-import './node_modules/decanter/core/src/scss/decanter.scss'
-// import 'decanter/core/src/scss/decanter'
-
-// Since fontawesome isn't working, trying to include the sections without fontawesome
-
-// import './node_modules/decanter/core/src/scss/utilities/variables/core/index.scss'
-//
-// colors is included in the index file but its apparently not finding it that way
-// import './node_modules/decanter/core/src/scss/utilities/variables/core/_colors.css'
-//
-// import './node_modules/decanter/core/src/scss/utilities/variables/elements/index.scss'
-// import './node_modules/decanter/core/src/scss/utilities/variables/components/index.scss'
-// core fonts inclusion here doesn't include fontawesome
-// import './node_modules/decanter/core/src/scss/core/fonts/fonts.scss'
-
-
-
-// or:
-// require('./src/styles/global.css')
+import "./src/styles/scss/global.scss"
