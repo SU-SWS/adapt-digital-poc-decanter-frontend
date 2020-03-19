b87793e1e948b5ea9ac753a7723571f9ea6a331f
Rebecca Hong <Rebeccahongsf@gmail.com>
2020-03-19 13:21:08 -0700

    Comment out gatsby-node.js contents

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,72 +1,76 @@
-const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
+// const path = require(`path`)
+// const { createFilePath } = require(`gatsby-source-filesystem`)
 
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions
 
-  const event = path.resolve(`./src/templates/blog-event.js`)
-  const result = await graphql(
-    `
-      {
-        allContentfulEvent {
-          edges {
-            node {
-              id
-              path
-            }
-          }
-        }
-        allMarkdownRemark(
-          sort: { fields: [frontmatter___date], order: DESC }
-          limit: 1000
-        ) {
-          edges {
-            node {
-              fields {
-                path
-              }
-              frontmatter {
-                title
-              }
-            }
-          }
-        }
-      }
-    `
-  )
+//   const event = path.resolve(`./src/templates/blog-event.js`)
+//   const result = await graphql(
+//     `
+//       query {
+//         allContentfulEvent {
+//           edges {
+//             node {
+//               id
+//               path
+//             }
+//           }
+//         }
+//         allMarkdownRemark(
+//           sort: { fields: [frontmatter___date], order: DESC }
+//           limit: 1000
+//         ) {
+//           edges {
+//             node {
+//               fields {
+//                 path
+//               }
+//               frontmatter {
+//                 title
+//               }
+//             }
+//           }
+//         }
+//       }
+//     `
+//   )
 
-  if (result.errors) {
-    throw result.errors
-  }
+//   if (result.errors) {
+//     throw result.errors
+//   }
 
-  // Create blog events pages.
-  const events = result.data.allContentfulEvent.edges
+//   // Create blog events pages.
+//   const events = result.data.allContentfulEvent.edges
 
-  events.forEach((event, index) => {
-    const previous = index === events.length - 1 ? null : events[index + 1].node
-    const next = index === 0 ? null : events[index - 1].node
+//   events.forEach((event, index) => {
+//     const previous = index === events.length - 1 ? null : events[index + 1].node
+//     const next = index === 0 ? null : events[index - 1].node
 
-    createPage({
-      path: event.node.path,
-      component: event,
-      context: {
-        slug: event.node.path,
-        previous,
-        next,
-      },
-    })
-  })
-}
+//     createPage({
+//       path: event.node.path,
+//       component: event,
+//       context: {
+//         slug: event.node.path,
+//         previous,
+//         next,
+//       },
+//     })
+//   })
+// }
 
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
+// exports.onCreateNode = ({ node, actions, getNode }) => {
+//   const { createNodeField } = actions
 
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
-    createNodeField({
-      name: `path`,
-      node,
-      value,
-    })
-  }
-}
+//   if (node.internal.type === `MarkdownRemark`) {
+//     const value = createFilePath({ 
+//       node, 
+//       getNode 
+//     })
+
+//     createNodeField({
+//       name: `path`,
+//       node,
+//       value,
+//     })
+//   }
+// }
